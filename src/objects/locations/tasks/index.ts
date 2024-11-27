34955cb26009f8e30c54c0139fa30676b6afcfc9
Kyle Buxton <kyle@callbackinc.com>
2024-11-26 21:21:15 -0700

    adding tasks and timezone methods to locations route

--- src/objects/locations/tasks/index.ts
+++ src/objects/locations/tasks/index.ts
@@ -1,3 +1,5 @@
+import search from "./search";
+
 export const tasks = {
-  search: () => {},
+  search,
 };
--- src/objects/locations/timezones/index.ts
+++ src/objects/locations/timezones/index.ts
@@ -1,3 +1,5 @@
+import search from "./search";
+
 export const timezones = {
-  list: () => {},
+  search,
 };
